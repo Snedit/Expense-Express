2a1151f0f6703a9c42532e2f2860fec834f47ef3
Snedit <sohamde2004@gmail.com>
2024-07-28 22:30:11 +0530

    fire and auth

---
+++ tracker/src/pages/auth/index.jsx
@@ -0,0 +1,9 @@
+function Auth() {
+  return (
+    <div className="login-page">
+      <p>Sign in with Google to Continue</p>
+      <button className="googleLogin">Sign in with Google</button>
+    </div>
+  );
+}
+export default Auth;
---
+++ tracker/src/pages/expense/index.jsx
@@ -0,0 +1,5 @@
+function ExpenseTracker() {
+  return <div>Goodnight expense</div>;
+}
+
+export default ExpenseTracker;
